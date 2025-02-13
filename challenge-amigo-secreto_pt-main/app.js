b441f58e9c4f201a5d61f4b528f9dce027be90a3
Maycon <maycon.tld@gmail.com>
2025-02-13 16:04:17 -0300

    Atualização final

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -8,6 +8,11 @@ let nomes = [];
 function adicionarAmigo() {
     let campoAmigo = document.getElementById('amigo'); // html.
     let amigo = campoAmigo.value;
+    if (amigo === '') {//Se estiver vazio, 
+        alert('Por favor, insira um nome.'); // exiba um alerta com a mensagem de erro.
+        return;
+    }
+
     nomes.push(amigo);// Add na lista.
     campoAmigo.value = ''; // limpa campo.
     campoAmigo.focus(); // retorna o cursor.
@@ -15,15 +20,27 @@ function adicionarAmigo() {
 
 //3. Função para atualizar lista.
 function updateLista() {
-    let listaAmigos = document.getElementById("listaAmigos");
+    let listaAmigos = document.getElementById("listaAmigos");// html.
     listaAmigos.innerHTML = "";
-      
+      //Crie uma função que percorra o array amigos e adicione cada nome como um elemento
+      //  <li> dentro de uma lista HTML. Use innerHTML para limpar a lista antes de 
+      // adicionar novos elementos.
     for (let numerico = 0; numerico < nomes.length; numerico++) {
         let li = document.createElement("li");
         li.textContent = nomes[numerico];
         listaAmigos.appendChild(li);
         }
-        console.log(nomes);    
+        // console.log(nomes);  ok  
 }
-updateLista();
-
+updateLista()
+//4. Implementa uma função para sortear os amigos.   
+function sortearAmigo() {
+    let sorteio= Math.floor(Math.random() * nomes.length);
+    let amigoSecreto = nomes[sorteio];
+    //console.log(sorteio);ok
+    
+    document.getElementById("resultado").innerHTML = `Seu Amigo Secreto é:  ${amigoSecreto}`// html.
+    //Mostrar o resultado: Atualizar o conteúdo do elemento de resultado usando document.getElementById() 
+    // e innerHTML para exibir o amigo sorteado.
+}
+//funcionando ok
