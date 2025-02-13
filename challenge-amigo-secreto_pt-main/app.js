e2cd9a6f8cd014c5aa1f92694eb81ef0bd868b2c
Maycon <maycon.tld@gmail.com>
2025-02-13 15:04:05 -0300

    Return and clear

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,14 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
 //Aqui você deverá desenvolver a lógica para resolver o problema.
 
-// 1.Array para armazenar nomes.
+//1. Array para armazenar nomes.
 let nomes = [];
 
-// 2.Função para agregar amigos.
+//2. Função para agregar amigos.
 function adicionarAmigo() {
     let campoAmigo = document.getElementById('amigo'); // html.
     let amigo = campoAmigo.value;
     nomes.push(amigo);// Add na lista.
-    console.log(nomes);    
+    console.log(nomes);   
+
+campoAmigo.value = ''; // limpa campo.
+campoAmigo.focus(); // retorna o cursor.
 }
-adicionarAmigo();
