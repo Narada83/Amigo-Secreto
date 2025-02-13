642964f664b4d3a4590cd1e5db0e5d4de949373f
Maycon <maycon.tld@gmail.com>
2025-02-13 14:31:36 -0300

    Criei array e função add nomes.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+//Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// 1.Array para armazenar nomes.
+let nomes = [];
+
+// 2.Função para agregar amigos.
+function adicionarAmigo() {
+    let campoAmigo = document.getElementById('amigo'); // html.
+    let amigo = campoAmigo.value;
+    nomes.push(amigo);// Add na lista.
+    console.log(nomes);    
+}
+adicionarAmigo();
